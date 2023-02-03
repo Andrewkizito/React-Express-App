c9c41473a471c921a47a494fd683fb731f54905d
AndrewKizito <AndrewKizito54@gmail.com>
2023-02-03 14:19:53 +0300

    Added Login Page

--- app/frontend/src/pages/admin/auth/Login.tsx
+++ app/frontend/src/pages/admin/auth/Login.tsx
@@ -1,11 +1,68 @@
-import { Box } from "@mui/material";
+import { Login } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  CSSObject,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+const LoginPage = () => {
+  const centerStyles: CSSObject = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "1rem",
+  };
+
   return (
-    <Box height="100vh" width="100vw" bgcolor="primary.main">
-      Login
+    <Box
+      display="flex"
+      justifyContent={"center"}
+      alignItems="center"
+      height="100vh"
+      width="100vw"
+      bgcolor="primary.main"
+    >
+      <Paper
+        sx={{
+          height: 400,
+          width: 500,
+          padding: "2rem 1.5rem",
+          ...centerStyles,
+        }}
+      >
+        <img src={"/logo.svg"} />
+        <Typography fontWeight={600} fontSize={"1.2rem"} variant="h4">
+          Sign In As Administrator.
+        </Typography>
+        <TextField fullWidth label="Username" />
+        <TextField fullWidth label="Password" />
+        <Box
+          sx={{
+            ...centerStyles,
+            width: "100%",
+            justifyContent: "space-between",
+            flexDirection: "row",
+          }}
+        >
+          <Link to="/" style={{ fontSize: "0.9rem", color: blue[600] }}>
+            Forgot Password
+          </Link>
+          <Link to="/" style={{ fontSize: "0.9rem" }}>
+            Not Admin, Leave Page
+          </Link>
+        </Box>
+        <Button endIcon={<Login />} fullWidth>
+          Login
+        </Button>
+      </Paper>
     </Box>
   );
 };
 
-export default Login;
+export default LoginPage;
